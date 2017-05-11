6dd2021354be58d91bae8dcdfffab27b09120c16
feilong <614183595@qq.com>
2017-05-11 12:29:03 +0000

    Thu 11 May 12:29:03 UTC 2017

---
+++ build/plugins/input_opencv/filters/cvfilter_py/CMakeFiles/cvfilter_py.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvfilter_py.dir/filter_py.cpp.o"
+  "CMakeFiles/cvfilter_py.dir/conversion.cpp.o"
+  "cvfilter_py.pdb"
+  "cvfilter_py.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvfilter_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
