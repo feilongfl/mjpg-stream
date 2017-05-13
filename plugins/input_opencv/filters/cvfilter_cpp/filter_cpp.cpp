0d25cf186645040461fefbc214ad15c8cf3b6db3
feilong <614183595@qq.com>
2017-05-13 14:41:29 +0800

    2017年 05月 13日 星期六 14:41:29 CST

--- plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
+++ plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
@@ -40,9 +40,9 @@ void filter_process(void* filter_ctx, Mat &src, Mat &dst) {
     //color
     int iLowH = 67;
     int iHighH = 140;
-    int iLowS = 10;
+    int iLowS = 5;
     int iHighS = 255;
-    int iLowV =10;
+    int iLowV =5;
     int iHighV = 255;
 
     cvtColor(src, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
