edfb7dd58239579508f85360d82768b9b672f22e
feilong <614183595@qq.com>
2017-05-17 10:30:31 +0800

    debug hsv

--- plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
+++ plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
@@ -295,7 +295,7 @@ void filter_process(void* filter_ctx, Mat &src, Mat &dst) {
 
 	try
 	{
-        HSVRange hsvR = {80,140,0,255,0,255};
+        HSVRange hsvR = {80,140,0,200,0,255};
 		calMat = ColorFinder(src,hsvR); //背景提取
         dst = calMat;
         return;
