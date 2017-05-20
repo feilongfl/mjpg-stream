bdb3d21715d048edd04b6277359e3d448d72b9f7
feilong <614183595@qq.com>
2017-05-20 18:17:37 +0800

    test

--- plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
+++ plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
@@ -277,7 +277,7 @@ lines_s2 lineFit1(vector<lines_s> lines)
 	for(size_t i = 0;i < linesNew.size() - 1;i ++)
 	{
 		for(size_t j = 0;j < linesNew[i].size() - 1;j ++) {
-			cout << linesNew[i][j].rho;
+			cout << linesNew[i][j].rho << "\t";
 		}
 		cout << endl;
 	}
