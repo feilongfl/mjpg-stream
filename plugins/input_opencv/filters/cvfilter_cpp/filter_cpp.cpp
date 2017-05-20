71675f0476ac57d08a931820538e8c82d4bfed5c
feilong <614183595@qq.com>
2017-05-20 14:16:04 +0800

    test hough

--- plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
+++ plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
@@ -248,7 +248,7 @@ Mat KeystoneCorrection(Mat src,Mat oriSrc,bool debug = false)//去除背景图�
 	//Mat canBgr;
 	//cvtColor(can, canBgr, COLOR_GRAY2BGR);
 	vector<Vec2f> lines;
-	HoughLines(can, lines, 1, CV_PI / 360, 50, 0, 0);
+	HoughLines(can, lines, 1, CV_PI / 360, 70, 0, 0);
 
 //////////////////////////////////////
     //debug
