1072853febe0cda4bf66836f8f6b4d53fd6fad51
feilong <614183595@qq.com>
2017-05-20 12:46:42 +0800

    canny test

--- plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
+++ plugins/input_opencv/filters/cvfilter_cpp/filter_cpp.cpp
@@ -245,7 +245,7 @@ Mat KeystoneCorrection(Mat src,Mat oriSrc,bool debug = false)//去除背景图�
 
     //return can;
 	Canny(src, can, 1, 3, 3);
-    //return can;
+    return can;
     //cvtColor(src,can,COLOR_BGR2GRAY);
 	//Mat canBgr;
 	//cvtColor(can, canBgr, COLOR_GRAY2BGR);
